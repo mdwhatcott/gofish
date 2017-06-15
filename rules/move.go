fe6eb9f2cbf0ba7643200d6c7664c7de61afd5f0
Whatcott, Michael <mdwhatcott@gmail.com>
2017-06-15 00:14:16 -0600

    Forward-thinking draft of the Move struct.

--- rules/move.go
+++ rules/move.go
@@ -1,7 +1,12 @@
 package rules
 
 type Move struct {
+	Piece Piece
 	From  Square
 	To    Square
-	Piece Piece
+
+	Captures   Piece
+	CapturesOn Square
+
+	Castles bool // When true, the Piece will represent the involved Rook.
 }
