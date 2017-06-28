d1cef798912e3e223ff2537e6597a45837bfdea3
Michael Whatcott <mdwhatcott@gmail.com>
2017-06-27 20:02:30 -0600

    Simplify assertion FEN strings.

--- rules/game_test.go
+++ rules/game_test.go
@@ -46,7 +46,7 @@ func (this *GameFixture) TestGameConditionsAfterTakingBackTheFirstMove() {
 }
 
 func (this *GameFixture) TestTakeBackPromotion() {
-	this.game.MustLoadFEN("8/8/8/8/8/7k/K3p3/8 b - - 0 1")
+	this.game.MustLoadFEN("8/8/8/8/8/7k/K3p3/8")
 	move := move{Piece: BlackPawn, From: Square("e2"), To: Square("e1"), Promotion: BlackQueen}
 	this.game.Execute(move)
 	this.game.TakeBack(move)
@@ -60,7 +60,7 @@ func (this *GameFixture) TestLoadFEN() { // TODO: test many more pieces and scen
 	this.So(this.game.ExportFEN(), should.StartWith, kingsOnBackRanks)
 }
 func (this *GameFixture) TestCheckMate() {
-	this.assertInCheckMate("7k/5KQ1/8/8/8/8/8/8 w - - 0 1", Black)
+	this.assertInCheckMate("7k/5KQ1/8/8/8/8/8/8", Black)
 }
 func (this *GameFixture) assertInCheckMate(position string, player player) {
 	this.game.MustLoadFEN(position)
@@ -68,45 +68,45 @@ func (this *GameFixture) assertInCheckMate(position string, player player) {
 	this.So(this.game.IsOver(), should.BeTrue)
 }
 func (this *GameFixture) TestCheckByPawnAggressor() {
-	this.assertInCheck("3k4/4P3/8/8/8/8/8/4K3 w - - 0 1", Black)
-	this.assertInCheck("3k4/2P5/8/8/8/8/8/4K3 w - - 0 1", Black)
-	this.assertNotInCheck("2k5/2P5/8/8/8/8/8/4K3 w - - 0 1", Black)
+	this.assertInCheck("3k4/4P3/8/8/8/8/8/4K3", Black)
+	this.assertInCheck("3k4/2P5/8/8/8/8/8/4K3", Black)
+	this.assertNotInCheck("2k5/2P5/8/8/8/8/8/4K3", Black)
 
-	this.assertInCheck("2k5/8/8/8/8/8/3p4/4K3 b - - 0 1", White)
-	this.assertInCheck("2k5/8/8/8/8/8/5p2/4K3 b - - 0 1", White)
-	this.assertNotInCheck("2k5/8/8/8/8/8/4p3/4K3 b - - 0 1", White)
+	this.assertInCheck("2k5/8/8/8/8/8/3p4/4K3", White)
+	this.assertInCheck("2k5/8/8/8/8/8/5p2/4K3", White)
+	this.assertNotInCheck("2k5/8/8/8/8/8/4p3/4K3", White)
 }
 func (this *GameFixture) TestCheckByKnightAggressor() {
-	this.assertInCheck("2k5/8/3N4/8/8/8/8/4K3 w - - 0 1", Black)
-	this.assertInCheck("2k5/4N3/8/8/8/8/8/4K3 w - - 0 1", Black)
-	this.assertNotInCheck("3k4/4N3/8/8/8/8/8/4K3 w - - 0 1", Black)
+	this.assertInCheck("2k5/8/3N4/8/8/8/8/4K3", Black)
+	this.assertInCheck("2k5/4N3/8/8/8/8/8/4K3", Black)
+	this.assertNotInCheck("3k4/4N3/8/8/8/8/8/4K3", Black)
 
-	this.assertInCheck("2K5/8/3n4/8/8/8/8/4k3 w - - 0 1", White)
-	this.assertInCheck("2K5/4n3/8/8/8/8/8/4k3 w - - 0 1", White)
-	this.assertNotInCheck("3K4/4n3/8/8/8/8/8/4k3 w - - 0 1", White)
+	this.assertInCheck("2K5/8/3n4/8/8/8/8/4k3", White)
+	this.assertInCheck("2K5/4n3/8/8/8/8/8/4k3", White)
+	this.assertNotInCheck("3K4/4n3/8/8/8/8/8/4k3", White)
 }
 func (this *GameFixture) TestCheckByRookAggressor() {
-	this.assertInCheck("3k4/8/8/8/8/8/3R4/3K4 w - - 0 1", Black)
-	this.assertInCheck("8/8/8/8/8/8/3r3K/3k4 b - - 0 1", White)
-	this.assertNotInCheck("8/8/8/8/8/8/3R2pk/3K4 w - - 0 1", Black)
-	this.assertNotInCheck("8/8/7K/8/8/8/3r4/3k4 b - - 0 1", White)
+	this.assertInCheck("3k4/8/8/8/8/8/3R4/3K4", Black)
+	this.assertInCheck("8/8/8/8/8/8/3r3K/3k4", White)
+	this.assertNotInCheck("8/8/8/8/8/8/3R2pk/3K4", Black)
+	this.assertNotInCheck("8/8/7K/8/8/8/3r4/3k4", White)
 }
 func (this *GameFixture) TestCheckByBishopAggressor() {
-	this.assertInCheck("7k/8/8/8/P7/8/8/B2K4 w - - 0 1", Black)
-	this.assertInCheck("b2k4/8/8/7p/8/8/8/7K b - - 0 1", White)
-	this.assertNotInCheck("8/8/8/8/P7/8/k7/B2K4 w - - 0 1", Black)
-	this.assertNotInCheck("b2k4/K7/8/7p/8/8/8/8 b - - 0 1", White)
+	this.assertInCheck("7k/8/8/8/P7/8/8/B2K4", Black)
+	this.assertInCheck("b2k4/8/8/7p/8/8/8/7K", White)
+	this.assertNotInCheck("8/8/8/8/P7/8/k7/B2K4", Black)
+	this.assertNotInCheck("b2k4/K7/8/7p/8/8/8/8", White)
 }
 func (this *GameFixture) TestCheckByQueenAggressor() {
-	this.assertInCheck("3k4/3q4/8/8/8/8/8/3K4 w - - 0 1", White)
-	this.assertInCheck("3k4/8/8/7q/8/8/8/3K4 w - - 0 1", White)
-	this.assertInCheck("3k4/8/8/8/8/8/8/2qK4 w - - 0 1", White)
-	this.assertNotInCheck("3k4/3q4/8/8/8/8/8/4K3 w - - 0 1", White)
+	this.assertInCheck("3k4/3q4/8/8/8/8/8/3K4", White)
+	this.assertInCheck("3k4/8/8/7q/8/8/8/3K4", White)
+	this.assertInCheck("3k4/8/8/8/8/8/8/2qK4", White)
+	this.assertNotInCheck("3k4/3q4/8/8/8/8/8/4K3", White)
 
-	this.assertInCheck("3K4/3Q4/8/8/8/8/8/3k4 b - - 0 1", Black)
-	this.assertInCheck("3K4/8/8/7Q/8/8/8/3k4 b - - 0 1", Black)
-	this.assertInCheck("3K4/8/8/8/8/8/8/2Qk4 b - - 0 1", Black)
-	this.assertNotInCheck("3K4/3Q4/8/8/8/8/8/4k3 b - - 0 1", Black)
+	this.assertInCheck("3K4/3Q4/8/8/8/8/8/3k4", Black)
+	this.assertInCheck("3K4/8/8/7Q/8/8/8/3k4", Black)
+	this.assertInCheck("3K4/8/8/8/8/8/8/2Qk4", Black)
+	this.assertNotInCheck("3K4/3Q4/8/8/8/8/8/4k3", Black)
 }
 func (this *GameFixture) assertInCheck(position string, color player) {
 	this.game.MustLoadFEN(position)
--- rules/pieces_pawn_test.go
+++ rules/pieces_pawn_test.go
@@ -21,44 +21,40 @@ func (this *PawnMovesFixture) Setup() {
 
 const (
 	pawnAdvancement                  = "7k/8/8/7p/P7/8/8/K7 "
-	whitePawnAdvancement             = pawnAdvancement + "w - - 0 1"
-	blackPawnAdvancement             = pawnAdvancement + "b - - 0 1"
 	blockedPawn                      = "7K/8/8/p7/P7/8/8/K7 "
-	whiteBlockedPawn                 = blockedPawn + "w - - 0 1"
-	blackBlockedPawn                 = blockedPawn + "b - - 0 1"
 	initialPawn                      = "k7/p7/8/8/8/8/P7/K7 "
-	whiteInitialPawnAdvancement      = initialPawn + "w - - 0 1"
-	blackInitialPawnAdvancement      = initialPawn + "b - - 0 1"
 	singlePawnCapture                = "k7/8/8/4p3/3P4/8/8/K7 "
-	whiteSinglePawnCapture           = singlePawnCapture + "w - - 0 1"
-	blackSinglePawnCapture           = singlePawnCapture + "b - - 0 1"
 	doublePawnCaptures               = "k7/8/8/n1n3p1/1P3N1N/8/8/K7 "
-	whiteDoublePawnCapture           = doublePawnCaptures + "w - - 0 1"
-	blackDoublePawnCapture           = doublePawnCaptures + "b - - 0 1"
-	whitePawnAdvancesToPromote       = "8/5P2/8/8/8/7k/8/1K6 w - - 0 1"
-	blackPawnAdvancesToPromote       = "8/8/K7/8/8/7k/4p3/8 b - - 0 1"
-	whitePawnCapturesAndPromotes     = "2br4/3P4/7k/K7/8/8/8/8 w - - 0 1"
-	blackPawnCapturesAndPromotes     = "8/8/7k/K7/8/8/4p3/4RN2 b - - 0 1"
-	pawnAdvancesToCheckEnemyKing     = "7k/8/6P1/6P1/8/8/8/1K6 w - - 0 1"
-	pawnCapturesToCheckEnemyKing     = "7k/6p1/5P2/8/8/8/8/1K6 w - - 0 1"
-	pawnPromotesToCheckEnemyKing     = "7k/5P2/8/8/8/8/8/1K6 w - - 0 1"
-	pawnPromotesToCheckmateEnemyKing = "7k/5P1p/8/8/8/8/8/1K6 w - - 0 1"
-	// TODO: pawnCanCheckmateEnemyKing (with advancement, capture, or promotion = Q or R or B or N)
-	// TODO: en-passant
+	whitePawnAdvancesToPromote       = "8/5P2/8/8/8/7k/8/1K6"
+	blackPawnAdvancesToPromote       = "8/8/K7/8/8/7k/4p3/8"
+	whitePawnCapturesAndPromotes     = "2br4/3P4/7k/K7/8/8/8/8"
+	blackPawnCapturesAndPromotes     = "8/8/7k/K7/8/8/4p3/4RN2"
+	pawnAdvancesToCheckEnemyKing     = "7k/8/6P1/6P1/8/8/8/1K6"
+	pawnCapturesToCheckEnemyKing     = "7k/6p1/5P2/8/8/8/8/1K6"
+	pawnPromotesToCheckEnemyKing     = "7k/5P2/8/8/8/8/8/1K6"
+	pawnPromotesToCheckmateEnemyKing = "7k/5P1p/8/8/8/8/8/1K6"
+	// TODO: en-passant move mechanics
+	// TODO: pawnCanCheckmateEnemyKing advancement
+	// TODO: pawnCanCheckmateEnemyKing capture
+	// TODO: pawnCanCheckmateEnemyKing promotion = Q
+	// TODO: pawnCanCheckmateEnemyKing promotion = R
+	// TODO: pawnCanCheckmateEnemyKing promotion = B
+	// TODO: pawnCanCheckmateEnemyKing promotion = N
+	// TODO: pawnCanCheckmateEnemyKingWithEnPassant
 
 )
 
 func (this *PawnMovesFixture) Test() {
-	this.assertLegalPieceMoves(whitePawnAdvancement, "a4", WhitePawn, "a5")
-	this.assertLegalPieceMoves(blackPawnAdvancement, "h5", BlackPawn, "h4")
-	this.assertLegalPieceMoves(whiteInitialPawnAdvancement, "a2", WhitePawn, "a3", "a4")
-	this.assertLegalPieceMoves(blackInitialPawnAdvancement, "a7", BlackPawn, "a6", "a5")
-	this.assertLegalPieceMoves(whiteBlockedPawn, "a4", WhitePawn)
-	this.assertLegalPieceMoves(blackBlockedPawn, "a5", BlackPawn)
-	this.assertLegalPieceMoves(whiteSinglePawnCapture, "d4", WhitePawn, "d5", "dxe5")
-	this.assertLegalPieceMoves(blackSinglePawnCapture, "e5", BlackPawn, "e4", "exd4")
-	this.assertLegalPieceMoves(whiteDoublePawnCapture, "b4", WhitePawn, "bxa5", "b5", "bxc5")
-	this.assertLegalPieceMoves(blackDoublePawnCapture, "g5", BlackPawn, "gxf4", "g4", "gxh4")
+	this.assertLegalPieceMoves(pawnAdvancement, "a4", WhitePawn, "a5")
+	this.assertLegalPieceMoves(pawnAdvancement, "h5", BlackPawn, "h4")
+	this.assertLegalPieceMoves(initialPawn, "a2", WhitePawn, "a3", "a4")
+	this.assertLegalPieceMoves(initialPawn, "a7", BlackPawn, "a6", "a5")
+	this.assertLegalPieceMoves(blockedPawn, "a4", WhitePawn)
+	this.assertLegalPieceMoves(blockedPawn, "a5", BlackPawn)
+	this.assertLegalPieceMoves(singlePawnCapture, "d4", WhitePawn, "d5", "dxe5")
+	this.assertLegalPieceMoves(singlePawnCapture, "e5", BlackPawn, "e4", "exd4")
+	this.assertLegalPieceMoves(doublePawnCaptures, "b4", WhitePawn, "bxa5", "b5", "bxc5")
+	this.assertLegalPieceMoves(doublePawnCaptures, "g5", BlackPawn, "gxf4", "g4", "gxh4")
 	this.assertLegalPieceMoves(pawnAdvancesToCheckEnemyKing, "g6", WhitePawn, "g7+")
 	this.assertLegalPieceMoves(pawnCapturesToCheckEnemyKing, "f6", WhitePawn, "f7", "fxg7+")
 	this.assertLegalPieceMoves(whitePawnAdvancesToPromote, "f7", WhitePawn, "f8=Q", "f8=R", "f8=B", "f8=N")
